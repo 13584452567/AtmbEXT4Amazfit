b44f266e8959dc1e421e56bc43ca0861d85f2bb1
Zicai <2831906503@qq.com>
2022-01-24 15:04:08 +0800

    Hello World

--- AtmbEXT4Amazfit/Class1.cs
+++ AtmbEXT4Amazfit/Class1.cs
@@ -1,8 +1,17 @@
-﻿using System;
+﻿using AutumnBox.OpenFramework.Extension;
+using AutumnBox.OpenFramework.Extension.Leaf;
+using AutumnBox.OpenFramework.Open;
 
-namespace AtmbEXT4Amazfit
+namespace AtmbExtDemo
 {
-    public class Class1
+    [ExtName("Hello world!")]
+    [ExtRequiredDeviceStates(AutumnBoxExtension.NoMatter)]
+    class EHelloAutumnBox : LeafExtensionBase
     {
+        [LMain]
+        public void Run(IUx ux)
+        {
+            ux.Message("你好，秋之盒");
+        }
     }
 }
