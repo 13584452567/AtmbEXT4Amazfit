6c37e207a7d7f7c9cb643edae77f6e06f8561b60
Zicai <2831906503@qq.com>
2022-01-25 13:52:29 +0800

    test

---
+++ AtmbEXT4Amazfit/modle - 复制(3).cs
@@ -0,0 +1,17 @@
+﻿using AutumnBox.OpenFramework.Extension;
+using AutumnBox.OpenFramework.Extension.Leaf;
+using AutumnBox.OpenFramework.Open;
+
+namespace AtmbExtDemo
+{
+    [ExtName("Hello world!")]
+    [ExtRequiredDeviceStates(AutumnBoxExtension.NoMatter)]
+    class EHelloAutumnBox : LeafExtensionBase
+    {
+        [LMain]
+        public void Run(IUx ux)
+        {
+            ux.Message("你好，秋之盒");
+        }
+    }
+}
